a1ef1629bf5daa8b312bd59137712b61ad76c98c
Lacra <lacramioara.oloeriu@gmail.com>
2024-09-28 18:36:27 +0200

    added the first ptogram

---
+++ 01-first-program.js
@@ -0,0 +1,6 @@
+console.log("You are writting text to your scree.")
+console.log("This is called 'standard output'.")
+console.log("Tis staff in the ( ) is a string.")
+console.log("Start a string with a double-quote.")
+console.log("Write what you want.")
+console.log("Then end with a double-quote.")
