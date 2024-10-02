c50199f94b98c7baec45a28f46bbb9762e349603
Lacra <lacramioara.oloeriu@gmail.com>
2024-10-02 20:24:16 +0200

    added height

---
+++ 04-variables.js
@@ -0,0 +1,15 @@
+// let is the best way to make a variable
+
+let name = 'Zed A. Shaw';
+let age = 43;
+let height = 74;
+let feet = Math.round(height / 12);
+let inches = height - (feet * 12);
+
+// you can use a variable as a parameter
+
+console.log ("Name:", name);
+console.log ( "Age:" , age);
+
+// you can also embet variables is stringd...¨
+console.log(`Height ${feet} feet  ${inches} inches.`);
