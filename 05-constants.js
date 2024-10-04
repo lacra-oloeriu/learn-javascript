994b6a1402829b5f397dd30459075a58569a0792
Lacra <lacramioara.oloeriu@gmail.com>
2024-10-04 18:20:32 +0200

    get an error

--- 05-constants.js
+++ 05-constants.js
@@ -3,3 +3,11 @@ const invincible = 94;
 let changeme = 82;
 
 console.log(`invincible=${invincible} but changeme=${changeme}`);
+
+// thid will work just fine
+changeme = 100;
+
+// uncomment this to see how you can't do it 
+ invincible = 100000
+
+console.log(`After change; invincible=${invincible} but changeme=${changeme}`);
