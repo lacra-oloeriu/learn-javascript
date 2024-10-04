05891b6a1f1fd1493ccdea1f57c0d9567d154a73
Lacra <lacramioara.oloeriu@gmail.com>
2024-10-04 18:21:46 +0200

    fexed the error

--- 05-constants.js
+++ 05-constants.js
@@ -8,6 +8,6 @@ console.log(`invincible=${invincible} but changeme=${changeme}`);
 changeme = 100;
 
 // uncomment this to see how you can't do it 
- invincible = 100000
+// invincible = 100000
 
 console.log(`After change; invincible=${invincible} but changeme=${changeme}`);
