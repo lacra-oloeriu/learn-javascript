fddbc79e71cc5558a885d89d7c1807ea4d110895
Lacra <lacramioara.oloeriu@gmail.com>
2024-10-05 14:36:49 +0200

    fixed bug using escapes secvences

--- 06-escape-sequences.js
+++ 06-escape-sequences.js
@@ -1,1 +1,7 @@
 let talking = "I'm not sure is she said, \"Hello \" or not.";
+let walking = 'I\' ll need to go \'cause you don\'t know.';
+
+// this strings are broken. Use escapes to fix the,.
+let height = "6'2";
+console.log("I'm not g'na  see y'all, for a while.");
+console.log('Bogdan a zis " Spalate pe \'dinti\'!"')
