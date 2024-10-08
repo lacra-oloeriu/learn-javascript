ac134276ae79498b8918f817354d363a9f25ec3c
Lacra <lacramioara.oloeriu@gmail.com>
2024-10-08 20:09:18 +0200

    added age and eyes color

--- 07-Promting-imput.js
+++ 07-Promting-imput.js
@@ -1,6 +1,9 @@
 const readline = require('readline-sync');
 
 let name = readline.question("What's your name?");
-let age = readline.question("What's your age?")
+let age = readline.question("What's your age?");
+let eyes = readline.question("What's your eye color?");
 
 console.log(`Your name is ${name}`);
+console.log(`Your age is ${age}`);
+console.log(`Your eyes are ${eyes}`);
