c09f2bf5dbe4835cc3ff81567d54a2028138f05a
Lacra <lacramioara.oloeriu@gmail.com>
2024-10-08 20:03:32 +0200

    install readline-sync

---
+++ 07-Promting-imput.js
@@ -0,0 +1,6 @@
+const readline = require('readline-sync');
+
+let name = readline.question("What's your name?");
+let age = readline.question("What's your age?")
+
+console.log(`Your name is ${name}`);
