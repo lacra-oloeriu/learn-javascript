06108fa35e02314fae1bae620d1921b4609a134b
Lacra <lacramioara.oloeriu@gmail.com>
2024-10-15 06:22:56 +0200

    added functions

---
+++ 11-functions.js
@@ -0,0 +1,8 @@
+const printPerson = (name, age) => {
+    console.log(`Hi ${name}, you are ${age} years old.`);
+};
+
+printPerson('Lacra', 51);
+printPerson('Bogdan', 46);
+printPerson('Calin', 16);
+printPerson('Cezar', 13);
