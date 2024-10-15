ce2da31c178ddc6f4816fdedcb3d55671e42dfb0
Lacra <lacramioara.oloeriu@gmail.com>
2024-10-15 07:36:20 +0200

    added pet name and age to the function

--- 11-functions.js
+++ 11-functions.js
@@ -8,4 +8,14 @@ printPerson('Calin', 16);
 printPerson('Cezar', 13);
 
 console.log('-------------pets-----------');
+
+const printPet = (owner_name, owner_age, pet_name, pet_age) => {
+    printPerson(owner_name, owner_age);
+    console.log(`That person owns ${pet_name} who is ${pet_age} years old`);
+};
+
+printPet('Lacra', 51, 'Mr.Scruffles' , 10);
+printPet('Bogdan', 46, 'Crazy', 2);
+printPet('Calin', 16, 'LizzyLizard', 1);
+printPet('Cezar', 13, 'Kong The Donkey', 20);
  
