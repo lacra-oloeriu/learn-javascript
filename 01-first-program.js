517f334ee4d8db35a2610d8b5a6815191e86c920
Lacra <lacramioara.oloeriu@gmail.com>
2024-10-19 16:46:04 +0200

    exercice mylog function

--- 01-first-program.js
+++ 01-first-program.js
@@ -4,3 +4,15 @@ console.log("Tis staff in the ( ) is a string.")
 console.log("Start a string with a double-quote.")
 console.log("Write what you want.")
 console.log("Then end with a double-quote.")
+
+const myLog = (message) => {
+    console.log(message);
+}
+
+const secondLog =(variable, message) =>{
+    console.log("Varable = ", variable);
+    console.log("Message = ", message);
+}
+
+myLog("Hello Bogdan");
+secondLog(10, "Not good");
