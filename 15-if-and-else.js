ded59bc1cc69f22c890c41eed6758e007b82946c
Lacra <lacramioara.oloeriu@gmail.com>
2024-10-23 13:41:53 +0200

    adde if and else file and rezolve the first condition

---
+++ 15-if-and-else.js
@@ -0,0 +1,12 @@
+let x = 20
+
+if ( x ===10) {
+    console.log("x is equal to 10:", x);
+
+} else if ( x < 10) {
+   console.log("x is less than 10:", x);
+
+} else {
+    console.log("x is greater than 10:", x);
+}
+
