51489866ac2735f5b2d0f85d4adccbb7b9c1a682
Lacra <lacramioara.oloeriu@gmail.com>
2024-10-24 19:48:50 +0200

    added module 2

--- 16-while-loop.js
+++ 16-while-loop.js
@@ -4,3 +4,15 @@ while (count > 0) {
     console.log(`count down ${count}`);
     count--;
 }
+
+//lets make a wile loop of module 2
+
+while( count < 10) {
+    if( count % 2 == 0) {
+        console.log(count, "EVEN");
+    } else {
+        console.log( count, "ODD");
+    }
+
+    count++;
+}
