5aea9a9e88559832d9bfd90aa612e7f9c461121d
Lacra <lacramioara.oloeriu@gmail.com>
2024-10-25 19:32:19 +0200

    added the while loop and change some vsribles

--- 17-sequences-for-loops.js
+++ 17-sequences-for-loops.js
@@ -1,1 +1,7 @@
-console.log(process.argv);
+let pigments = ["perinone", "cadmium","titanium", "ultramarine", "napthol" ];
+
+let i = 0;
+while (i < pigments.length) {
+    console.log(`while ${i}=${pigments[i]}`);
+    i++;
+}
