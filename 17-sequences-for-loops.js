c30a8dbc2dfd2ec5f9ccf60da3d118b450819c58
Lacra <lacramioara.oloeriu@gmail.com>
2024-10-25 19:37:20 +0200

    used for loop

--- 17-sequences-for-loops.js
+++ 17-sequences-for-loops.js
@@ -5,3 +5,9 @@ while (i < pigments.length) {
     console.log(`while ${i}=${pigments[i]}`);
     i++;
 }
+
+//let's try somthing else
+
+for(let i = 0; i < pigments.length; i++ ) {
+    console.log(`for ${i}=${pigments[i]}`);
+}
