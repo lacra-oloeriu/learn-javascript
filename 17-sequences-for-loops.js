decf605afc41d71a171b115e7e3bfb81a1e1e45e
Lacra <lacramioara.oloeriu@gmail.com>
2024-10-26 08:19:41 +0200

    try to solve puzzel

--- 17-sequences-for-loops.js
+++ 17-sequences-for-loops.js
@@ -17,3 +17,29 @@ for(let i = 0; i < pigments.length; i++ ) {
 for(let paint of  pigments) {
     console.log(`for-of ${paint}`);
 }
+
+let count = 5;
+label1:
+//label2;
+while(count > 0) {
+    console.log("in while", count);
+    if(count > 2) {
+        for(i = 0; i < 4; i++) {
+            console.log("in for", i);
+            if(i == 2) {
+                count--;
+                continue label1;
+            }
+        }
+    }else if(count == 2) {
+        count -= 2;
+        console.log("continue in while");
+        continue label1;
+    } else {
+        break;
+    }
+    count--; 
+}
+
+//label2;
+console.log("Done.");
