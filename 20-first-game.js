bb7127a5d782a39b70f97aa7abfa2dcb312e0e87
Lacra <lacramioara.oloeriu@gmail.com>
2024-11-01 05:58:03 +0100

    adde the file 20 and start the game

---
+++ 20-first-game.js
@@ -0,0 +1,6 @@
+const readline = require('readline-sync');
+//prompt method allows you to display a dialog ox to the user and prompt them for imput.It takes as parameter the message that you want to diaplay
+
+const say = (prompt) => {
+    console.log(prompt);
+}
