34c6ea4ed72ab30a31f19a87eae4664d88e48dc5
Lacra <lacramioara.oloeriu@gmail.com>
2024-12-06 14:30:31 +0100

    added same logic

--- 20-first-game-v2.js
+++ 20-first-game-v2.js
@@ -5,6 +5,12 @@ const say = (prompt) => {
 
 const ask = (hp, prompt) => {
     console.log(`[[You have ${hp} hit points.]]`);
+    //let's put some logic in here 
+    if(hp <= 0) {
+        die("you died!");
+    } else {
+        return (prompt)
+    }
 }
 
 const well = (hp) => {
@@ -12,6 +18,10 @@ const well = (hp) => {
     say("Walking up to it ans looking down");
     say("You see a shiny thing at the bottom");
     let next = ask(hp, "What do you do?");
+    if ( next === "climb") {
+        say("You climb down the rope.");
+        rope (hp);
+    }
     
 }
 
