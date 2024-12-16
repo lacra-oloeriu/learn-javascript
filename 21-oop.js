d8c6ce83e780625660dd97c9227d7a51c2ce075c
Lacra <lacramioara.oloeriu@gmail.com>
2024-12-16 22:05:35 +0100

    added mary

--- 21-oop.js
+++ 21-oop.js
@@ -1,34 +1,3 @@
-//data about a person named Frank
-
-let frank = {
-    name:"Frank",
-    age: 34,
-    eyes: "blue"
-
-};
-
-const frank_talk = ( who, words) => {
-    console.log(` I am ${who.name} and ${words}`);
-};
-
-frank_talk(frank, "I am talking here!!");
-
-//working with that data by putting function on it.
-
-let mary = {
-    name: "Mary",
-    age: 44,
-    eyes: "brown", 
-    talk: ( who, words) => {
-        console.log(`I am ${who.name} and ${words}.`)
-    }
-};
-
-// now we taste it 
-
-mary.talk(mary, "these are some worlds");
-
-// an elegant way to buikd these automatically
 
 const Person = (name, age, eyes) => {
     //making an obj for the data
@@ -40,19 +9,23 @@ const Person = (name, age, eyes) => {
     // then attach our function to it
     obj.talk = (words) => {
         //obj here will keep a reference 
-        console.log(`I am ${obj.name}, I am ${obj.age}, I have green eyes ,  ${words}`);
+        console.log(`I am ${obj.name}, I am ${obj.age}, and  ${words}.`);
     }
 
     obj.walk = (distance, direction) => {
-        console.log(`${obj.name} walked`, distance, "in the direction of", direction)
+        console.log(`${obj.name} walked`, distance, "in the direction of", direction);
     }
     //and return our new person
-    return obj
+    return obj;
 }
 
 // let's use that to make someone named alex
 let alex = Person("Alex", 16, "green");
+let mary = Person("Mary", 90, "blue");
 // and see how they can talk without repetition?
-alex.talk ("Hi there!");
 
+
+alex.talk ("Hi there!");
 alex.walk("200m", "north");
+mary.talk("Hello");
+mary.walk("150m", "sud");
