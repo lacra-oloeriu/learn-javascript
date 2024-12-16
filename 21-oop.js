667df5d91eddd47a7a69ccdba41281a6baa89a8e
Lacra <lacramioara.oloeriu@gmail.com>
2024-12-16 18:24:32 +0100

    added oop file

---
+++ 21-oop.js
@@ -0,0 +1,12 @@
+let frank = {
+    name:"Frank",
+    age: 34,
+    eyes: "blue"
+
+};
+
+const frank_talk = ( who, worlds) => {
+    console.log(` I am ${who.name} and ${worlds}`);
+};
+
+frank_talk(frank, "I am talking here!!")
