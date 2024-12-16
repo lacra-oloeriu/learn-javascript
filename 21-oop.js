92dda6977904e811031f5b321bbb9299df6d11cd
Lacra <lacramioara.oloeriu@gmail.com>
2024-12-16 18:59:13 +0100

    added a constant in a frunction

--- 21-oop.js
+++ 21-oop.js
@@ -1,3 +1,5 @@
+//data about a person named Frank
+
 let frank = {
     name:"Frank",
     age: 34,
@@ -9,4 +11,19 @@ const frank_talk = ( who, worlds) => {
     console.log(` I am ${who.name} and ${worlds}`);
 };
 
-frank_talk(frank, "I am talking here!!")
+frank_talk(frank, "I am talking here!!");
+
+//working with that data by putting function on it.
+
+let mary = {
+    name: "Mary",
+    age: 44,
+    eyes: "brown", 
+    talk: ( who, worlds) => {
+        console.log(`I am ${who.name} and ${worlds}.`)
+    }
+};
+
+// now we taste it 
+
+mary.talk(mary, "these are some worlds");
