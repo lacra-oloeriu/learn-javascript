7077de681d16619d4cc0595320941e7f9c837789
Lacra <lacramioara.oloeriu@gmail.com>
2024-12-17 19:14:22 +0100

    added jon

--- 21-oop.js
+++ 21-oop.js
@@ -22,6 +22,9 @@ const Person = (name, age, eyes) => {
 // let's use that to make someone named alex
 let alex = Person("Alex", 16, "green");
 let mary = Person("Mary", 90, "blue");
+let jon = Person("Jon",35, "brown");
+let joseph = Person("Joseph", 19, "green");
+let anne = Person("Anne", 16,"blue");
 // and see how they can talk without repetition?
 
 
@@ -29,3 +32,5 @@ alex.talk ("Hi there!");
 alex.walk("200m", "north");
 mary.talk("Hello");
 mary.walk("150m", "sud");
+jon.talk("I am new!");
+jon.walk("450", "Vest");
