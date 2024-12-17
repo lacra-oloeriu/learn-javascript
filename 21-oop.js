ec5c4783704e421edcf0855ec0ce66878886ddbf
Lacra <lacramioara.oloeriu@gmail.com>
2024-12-17 19:17:42 +0100

    added joseph

--- 21-oop.js
+++ 21-oop.js
@@ -25,6 +25,7 @@ let mary = Person("Mary", 90, "blue");
 let jon = Person("Jon",35, "brown");
 let joseph = Person("Joseph", 19, "green");
 let anne = Person("Anne", 16,"blue");
+let zed = Person("Zed", 45, "green");
 // and see how they can talk without repetition?
 
 
@@ -33,4 +34,6 @@ alex.walk("200m", "north");
 mary.talk("Hello");
 mary.walk("150m", "sud");
 jon.talk("I am new!");
-jon.walk("450", "Vest");
+jon.walk("450m", "Vest");
+joseph.talk("I am from Gent!");
+joseph.walk("700m", "Vest");
