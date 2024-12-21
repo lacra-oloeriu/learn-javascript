abef8797719decfbf5a47c1735a4f5c0cbf43cf9
Lacra <lacramioara.oloeriu@gmail.com>
2024-12-21 07:32:54 +0100

    added frank

--- 22-complex-oop.js
+++ 22-complex-oop.js
@@ -16,7 +16,9 @@ let alex = Person("Alex", 18, "green");
  alex.talk("I am a student");
  let mary = Person("Mary", 54, "blue");
  mary.talk("I am human!!");
+ let frank = Person("Frank", 46, "black");
+ frank.talk("Hi there!")
 
- console.log (`Alex is ${alex.age}, Mary's ${mary.age}`);
+ console.log (`Alex is ${alex.age}, Mary's ${mary.age}, Frank's ${frank.age}`);
 
  // the same code but using class constructor
