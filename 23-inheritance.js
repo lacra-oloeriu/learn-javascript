d9faf00ebbf708fe886359d9472037b0572a5b27
Lacra <lacramioara.oloeriu@gmail.com>
2024-12-22 08:17:07 +0100

    undenstanding the inhereting

--- 23-inheritance.js
+++ 23-inheritance.js
@@ -10,9 +10,6 @@ class Guitar {
         //console.log("Playing", note)
         console.log(`${this.name}: ${this.color}: ${this.wood}: plays ${note}`)
     }
-
-
-
 }
 
 
@@ -27,9 +24,33 @@ class Telecaster extends Guitar {
 }
 
 
-//let g1 = new Guitar();
-//g1.play("A");
-
+let g1 = new Guitar();
+g1.play("A");
 let t1 = new Telecaster
 t1.volum ("L")
 t1.tone("L1")
+
+class Person {
+    constructor(name, age, eyes) {
+        this.name = name;
+        this.age = age;
+        this.eyes = eyes;
+    }
+
+    talk(words) {
+        console.log(`I am ${this.name} and ${words}.`)
+    }
+ }
+
+
+class Musician extends Person {
+    play(instrument, sheet_music) {
+        for(let note of sheet_music) {
+            instrument.play(note)
+        }
+    }
+}
+
+let alex = new Musician("Alex" , 16 , "green" )
+alex.talk("I am playing some tones")
+
