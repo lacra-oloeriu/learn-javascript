bd37e3d2ff3956d53544b69e5367219f41831f10
Lacra <lacramioara.oloeriu@gmail.com>
2024-12-24 07:17:20 +0100

    added the first class

--- 24-OOP-Game.js
+++ 24-OOP-Game.js
@@ -10,3 +10,19 @@ const die = (message) => {
     process.exit(1);
 }
 
+class Game {
+    constroctor(rooms) {
+        this.hp = Math.floor(Math.random() * 10) + 1;
+        this.addRooms(rooms)
+    }
+
+    ask(prompt) {
+        say(`[[You have ${this.hp} hit points.]]`);
+        if(this.hp <= 0) {
+            die("You died");
+        } else {
+            return readline.question(prompt + '')
+        }
+    }
+}
+
