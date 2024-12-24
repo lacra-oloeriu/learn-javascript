506731d0ca39c4471facb788824002fb482df37c
Lacra <lacramioara.oloeriu@gmail.com>
2024-12-24 07:10:10 +0100

    added const die

--- 24-OOP-Game.js
+++ 24-OOP-Game.js
@@ -3,3 +3,10 @@ const readline = require('readline-sync');
 const say = (prompt) => {
     console.log(prompt)
 }
+say("I started the game")
+
+const die = (message) => {
+    say(message);
+    process.exit(1);
+}
+
