acb5c91e80cb45ad02a52abb908f472d0ee2ed6f
Lacra <lacramioara.oloeriu@gmail.com>
2024-12-25 09:35:23 +0100

    added the first classs

--- 24-OOP-Game.js
+++ 24-OOP-Game.js
@@ -1,27 +1,51 @@
 const readline = require('readline-sync');
 
 class Game {
-    constroctor(rooms) {
+    constroctor() {
         this.hp = Math.floor(Math.random() * 10) + 1;
-        this.addRooms(rooms)
+        //this.addRooms(rooms)
     }
-}
 
 
 
-
-
-const say = (prompt) => {
+ say (prompt) {
     console.log(prompt)
 }
 
 
-say("I started the game")
+//say("I started the game")
 
-const die = (message) => {
+ die (message)  {
     say(message);
     process.exit(1);
 }
+
+ask(prompt) {
+    console.log(`[[You have $${this.hp} hit points.]]`);
+    if(this.hp <= 0) {
+        this.die("You died!");
+    } else {
+        return readline.question(prompt + ' ')
+    }
+} 
+
+addRoom(room) {
+    this[room.name] = room;
+    room.game = this;
+}
+
+play(name) {
+    this[name].enter();
+}
+
+hit(amount) {
+    this.hp -=amount;
+}
+
+}
+
+
+
 
 
 
