51c60c26e40b7a30e97341ab96d5a6f52c6f5ece
Lacra <lacramioara.oloeriu@gmail.com>
2024-12-25 09:21:09 +0100

    fixed code

--- 24-OOP-Game.js
+++ 24-OOP-Game.js
@@ -1,8 +1,21 @@
 const readline = require('readline-sync');
 
+class Game {
+    constroctor(rooms) {
+        this.hp = Math.floor(Math.random() * 10) + 1;
+        this.addRooms(rooms)
+    }
+}
+
+
+
+
+
 const say = (prompt) => {
     console.log(prompt)
 }
+
+
 say("I started the game")
 
 const die = (message) => {
@@ -10,19 +23,7 @@ const die = (message) => {
     process.exit(1);
 }
 
-class Game {
-    constroctor(rooms) {
-        this.hp = Math.floor(Math.random() * 10) + 1;
-        this.addRooms(rooms)
-    }
 
-    ask(prompt) {
-        say(`[[You have ${this.hp} hit points.]]`);
-        if(this.hp <= 0) {
-            die("You died");
-        } else {
-            return readline.question(prompt + '')
-        }
-    }
-}
+
+
 
