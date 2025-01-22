48023723ed6e0aa504ed26b5ff75e2604fd4f29a
Lacra <lacramioara.oloeriu@gmail.com>
2025-01-22 20:11:00 +0100

    added transforming data file

---
+++ 26-transforming-data.js
@@ -0,0 +1,20 @@
+// map =is apply a caclulation on each element
+// reduce - turn a sequance into a final calculation
+//find - search for an element in a sequence
+//filter - filter out selected elements from a sequence
+//
+
+class Thing {
+    doit() {
+        //does the stuff
+    }
+}
+
+let that = new Thing();
+that.doit();
+
+let x = [ 1, 2, 3, 4];
+x.map((i) => i + 10);
+console.log(x)
+
+//adder(x);
