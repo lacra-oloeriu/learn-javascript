77b7047b169530aedd6ce8ecc112904ca608480a
Lacra <lacramioara.oloeriu@gmail.com>
2025-01-25 17:57:45 +0100

    added forEach

--- 26-transforming-data.js
+++ 26-transforming-data.js
@@ -51,6 +51,20 @@ console.log(meaw);
 
 
 let old_animals = pets.filter(pet => pet.age > 10);
-let yoyng_animals = pets.filter( pet => pet.age <= 10);
+let young_animals = pets.filter( pet => pet.age <= 10);
 
 console.log( " Animal Ages:" , ages_only);
+
+console.log("Total Age:", total_age);
+
+console.log("Meaw is:", meaw.age);
+
+console.log("\nOld Animals:");
+old_animals.forEach(pet => {
+  console.log(`\t${pet.name} is a ${pet.age} old ${pet.type}.`);
+});
+
+console.log("\nYoung Animals:");
+for(let pet of young_animals) {
+  console.log(`\t${pet.name} is a ${pet.age} old ${pet.type}.`);
+}
