aa244f9889b8fd811de3eea92420de5cbe538608
Lacra <lacramioara.oloeriu@gmail.com>
2025-01-29 09:31:10 +0100

    fixed the bug

--- 28-scope-closures.js
+++ 28-scope-closures.js
@@ -1,4 +1,15 @@
-const var_stuck = () => {
+const var_sucks = () => {
     var var_scope = 10;
     let let_scope = 20;
+
+
+if(true) {
+    var var_scope = 100;
+    let let_scope = 100;
+    console.log(`>>> var_sucks inside if: var=${var_scope}, let=${let_scope}`);
+  }
+
+  console.log(`>>> var_sucks after if: var=${var_scope}, let=${let_scope}`);
 }
+
+var_sucks();
