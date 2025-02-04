93da2682e7ea88f8c8d878fd021615e6f8972976
Lacra <lacramioara.oloeriu@gmail.com>
2025-02-04 17:32:40 +0100

    added the to do list

---
+++ 30-callback-events-promises-async.js
@@ -0,0 +1,9 @@
+// doing simple things
+//1.open a file.
+//2.Get is size.
+//3.Create a buffer for that size.
+//4.Read the contents of the file into the buffe
+// A buffer is a space in memory (typically RAM) that stores binary data. 
+// In Node. js, we can access these spaces of memory with the built-in Buffer class.
+//  Buffers store a sequence of integers, similar to an array in JavaScript. 
+// Unlike arrays, you cannot change the size of a buffer once it is created.
