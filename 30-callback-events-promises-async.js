d7db8310d7f077c6dc292331f8dbabe4e883b905
Lacra <lacramioara.oloeriu@gmail.com>
2025-02-05 18:24:13 +0100

    added callback hell

--- 30-callback-events-promises-async.js
+++ 30-callback-events-promises-async.js
@@ -10,20 +10,55 @@
 
 //callbackstyle
 
-const fs = require('fs') ;
+//const fs = require('fs') ;
 
-const read_file = (fname, cb) => {
+//const read_file = (fname, cb) => {
+  //  fs.stat(fname, (err, stats) => {
+      //  fs.open(fname, 'r', (err, fd) => {
+          //  let inbuf = Buffer.alloc(stats.size);
+            //fs.read(fd, inbuf, 0, stats.size, null, (err, bytesRead, buffer ) => {
+              //  cb(buffer);
+           // })
+        //})
+   // })
+//}
+
+
+//read_file('test.txt', (result) => {
+   // console.log(`Result is ${result.toString()}`);
+  //});
+
+  //CallBack Hell
+
+  const fs = require('fs');
+
+  const read_file = (fname, cb) => {
     fs.stat(fname, (err, stats) => {
+      if(err) {
+        cb(err, null);
+      } else {
         fs.open(fname, 'r', (err, fd) => {
+          if(err) {
+            cb(err, null);
+          } else {
             let inbuf = Buffer.alloc(stats.size);
-            fs.read(fd, inbuf, 0, stats.size, null, (err, bytesRead, buffer ) => {
-                cb(buffer);
-            })
-        })
-    })
-}
-
-
-read_file('test.txt', (result) => {
-    console.log(`Result is ${result.toString()}`);
+            fs.read(fd, inbuf, 0, stats.size, null, (err, bytesRead, buffer) => {
+              if(err) {
+                cb(err, null);
+              } else {
+                cb(err, buffer);
+              }
+            });
+          }
+        });
+      }
+    });
+  }
+  
+  read_file('test.txt', (err, result) => {
+    if(err) {
+      console.log(err);
+    } else {
+      console.log(`Result is ${result.toString()}`);
+    }
   });
