78210c2c14486d4223138da393ff218fdf7f5fa0
Lacra <lacramioara.oloeriu@gmail.com>
2025-02-06 19:24:59 +0100

    defined the constants to read  open and stat the file

--- 30.1-event-style.js
+++ 30.1-event-style.js
@@ -1,2 +1,8 @@
 const fs = require('fs');
 
+const read_file = (fname, events) => {
+    let noop = () => {}
+    let onStat = events.onStat || noop;
+    let onOpen = events.onOpen|| noop;
+    let onRead = events.onRead || noop;
+}
