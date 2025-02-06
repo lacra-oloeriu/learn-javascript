ff66ef2401fbca4c090c4d9e62c06daa06600244
Lacra <lacramioara.oloeriu@gmail.com>
2025-02-06 19:39:30 +0100

    added onRead and onOpen functions

--- 30.1-event-style.js
+++ 30.1-event-style.js
@@ -5,8 +5,23 @@ const read_file = (fname, events) => {
     let onStat = events.onStat || noop;
     let onOpen = events.onOpen|| noop;
     let onRead = events.onRead || noop;
-
+    
     fs.stat(fname, (err, stats) => {
         onStat(stats);
-    })
+    
+        fs.open(fname, 'r', (err, fd) => {
+          onOpen(fd);
+          let inbuf = Buffer.alloc(stats.size);
+    
+          fs.read(fd, inbuf, 0, stats.size, null, (err, bytesRead, buffer) => {
+            onRead(bytesRead, buffer);
+          });
+        });
+      });
+
+
+
 }
+
+
+   
