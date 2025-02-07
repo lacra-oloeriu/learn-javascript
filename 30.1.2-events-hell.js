40554b1863c7525923862454fb4e29841bad7a8c
Lacra <lacramioara.oloeriu@gmail.com>
2025-02-07 21:38:32 +0100

    added the fierst condition  for errors

--- 30.1.2-events-hell.js
+++ 30.1.2-events-hell.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 
+//the code is practicly the same but whit errors added conditionly
+
 const read_file = (fname, events) => {
     let noop = () => {};
     let onError = events.onError || noop;
@@ -7,3 +9,11 @@ const read_file = (fname, events) => {
     let onOpen = events.onOpen || noop;
     let onRead = events.onRead || noop;
 }
+
+fs.stat(fname, (err, stats) => {
+    if(err) {
+        onError(err);
+    }else {
+        onStat(stats);
+    }
+})
