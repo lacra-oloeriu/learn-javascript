b15ab1a00deb596e37a24c20ee6fd8522368c61b
Lacra <lacramioara.oloeriu@gmail.com>
2025-02-09 11:59:54 +0100

    adde promise style

---
+++ 30.2-promise-style.js
@@ -0,0 +1,8 @@
+const sleeper = (timeout) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => resolve('DONE') , timeout);
+    })
+}
+
+let wait1 = sleeper(100);
+wait1.then(x => console.log("Done,", x));
