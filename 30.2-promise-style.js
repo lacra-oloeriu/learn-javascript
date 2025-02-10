f771c216d9337b940a9b41d2eda89eddc269af59
Lacra <lacramioara.oloeriu@gmail.com>
2025-02-10 06:42:04 +0100

    added catch error

--- 30.2-promise-style.js
+++ 30.2-promise-style.js
@@ -11,7 +11,7 @@ wait1.then(x => console.log("Done,", x));
 
 // next I will use promise to rewrite our code.
 
-const fs = require('fs').promoses;
+const fs = require('fs').promises;
 
 // you have to do nested calls any time  you need the result.....
 
@@ -19,8 +19,17 @@ const read_file = (fname) => {
     fs.open(fname, 'r').then((fh) => {
         fh.stat().then((stat) => {
             let buf = Buffer.alloc(stat.size);
-            
-        })
 
-    })
+            fh.read(buf, 0, stat.size, null)
+            .then((result) => {
+                console.log(`Read ${result.bytesRead} bytes: ${result.buffer.toString()}`)
+                
+            }).catch((err) => {
+                console.error(err);
+            });
+        }).catch((err) => console.error(err));
+
+    }).catch((err) => console.error(err));
 }
+
+read_file('test.txt');
