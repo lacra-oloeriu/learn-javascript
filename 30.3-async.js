c5275ac4d29af79525bb2adb537458b4c8f3a5da
Lacra <lacramioara.oloeriu@gmail.com>
2025-02-12 17:39:28 +0100

    created the fist constants

--- 30.3-async.js
+++ 30.3-async.js
@@ -1,7 +1,13 @@
 let fs = require('fs').promises
 
 const read_file = async (fname) => {
-    
+    try {
+        let file = await fs.open(fname, 'r');
+        let stat = await file.stat();
+        
+    } catch(err) {
+        console,log("ERROR", err);
+    }
 
 }
 
