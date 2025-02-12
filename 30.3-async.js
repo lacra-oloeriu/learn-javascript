c2519c6c43c684db18448ee981c69d243fba4f98
Lacra <lacramioara.oloeriu@gmail.com>
2025-02-12 17:43:53 +0100

    use exception to catch errors

--- 30.3-async.js
+++ 30.3-async.js
@@ -4,9 +4,12 @@ const read_file = async (fname) => {
     try {
         let file = await fs.open(fname, 'r');
         let stat = await file.stat();
-        
+        let buffer = Buffer.alloc(stat.size);
+        const result = await file.read(buffer, 0, stat.size, null);
+        console.log(`Read ${result.bytesRead} bytes: ${result.buffer.toString()}`);
+
     } catch(err) {
-        console,log("ERROR", err);
+        console.log("ERROR", err);
     }
 
 }
