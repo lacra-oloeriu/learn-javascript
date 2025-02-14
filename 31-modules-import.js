940901e2e8c7df67872d676781f372c1d0ebf433
Lacra <lacramioara.oloeriu@gmail.com>
2025-02-14 21:51:16 +0100

    imported the geometry file

--- 31-modules-import.js
+++ 31-modules-import.js
@@ -1,7 +1,1 @@
-const area = (r) => Math.PI * r ** 2;
-const circumference = (r) => 2 * Math.PI * r;
-
-module.exports = {
-    area: area,
-    circumference: circumference
-}
+const geometry = require ('./13-geometry')
